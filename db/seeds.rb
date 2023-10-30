bdf1378df2150cdd79f219efef16828ee24e3f24
Sammy Henningsson <sammy.henningsson@hey.com>
2023-10-30 06:24:36 +0100

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 require 'faker'
 
 5.times do
-  user = User.create(name: Faker::Name.first_name)
+  name = Faker::Name.first_name
+  loop do
+    break if name.size > 2
+    name = Faker::Name.first_name
+  end
+
+  user = User.create(name: name)
   rand(10).times do
     user.add_post(
       title: Faker::TvShows::FamilyGuy.character,
